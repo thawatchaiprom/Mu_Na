ff721e5b74ae6457459648eac19736ebc84ea448
thawatchaiprom <thawatchai_prom@hotmail.com>
2018-04-19 15:58:32 +0700

    Add files via upload

--- report_qrcode.aspx.cs
+++ report_qrcode.aspx.cs
@@ -14,52 +14,85 @@ using Microsoft.Reporting.WebForms;
 using System.Drawing.Imaging;
 using KeepAutomation.Barcode.Bean;
 using QRCoder;
+using ZXing;
+using Spire.Barcode.Implementation.Generator;
 using System.Drawing;
+
 
 namespace StockMUNA
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
-                spire_barcode();
+               // barcode();
                 genqr();
 
-                if (!IsPostBack)
+
+    
+
+
+
+            if (!IsPostBack)
                 {
 
+                /*  //set Processing Mode of Report as Local   
+                  ReportViewer1.ProcessingMode = ProcessingMode.Local;
+                  //set path of the Local report 
+                  // ReportViewer1.LocalReport.ReportPath = Server.MapPath("ReportQrcode.rdlc");
+                  ReportViewer1.LocalReport.ReportEmbeddedResource = "StockMUNA.ReportQrcode.rdlc";
+
+                  //creating object of DataSet dsmember and filling the DataSet using SQLDataAdapter 
+
+              DataSetStock dsMember = new DataSetStock();
+              MySqlConnection cn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["connection_stock"].ConnectionString);
+              cn.Open();
+              MySqlDataAdapter adapt = new MySqlDataAdapter("select stock_code,inv_no from stock_item where stock_code = 'TPS-009-002-001' ", cn);
+              adapt.Fill(dsMember, "DataTableQrcode"); //DataTableQrcode DataSetQrcode
+              cn.Close();
+
+                  //Providing DataSource for the Report   
+                  ReportDataSource rds = new ReportDataSource("DataSetQrcode", dsMember.Tables[0]);
+                  ReportViewer1.LocalReport.DataSources.Clear();
+                  //Add ReportDataSource   
+                  ReportViewer1.LocalReport.DataSources.Add(rds);*/
+
+
+                //set Processing Mode of Report as Local   
+                ReportViewer1.ProcessingMode = ProcessingMode.Local;
+                //set path of the Local report 
+                // ReportViewer1.LocalReport.ReportPath = Server.MapPath("ReportQrcode.rdlc");
+                ReportViewer1.LocalReport.ReportEmbeddedResource = "StockMUNA.ReportQrcode.rdlc";
+
+                DataSetStock dsMember = new DataSetStock();
+                MySqlConnection cn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["connection_stock"].ConnectionString);
+                cn.Open();
+                MySqlDataAdapter adapt = new MySqlDataAdapter("select stock_code,inv_no from stock_item  ", cn);
+                adapt.Fill(dsMember, "DataTableQrcode"); //DataTableQrcode DataSetQrcode
+
+                // Create and setup an instance of Bytescout Barcode SDK
+
+
+                // Update DataTable with barcode image
+                foreach (DataSetStock.DataTableQrcodeRow row in dsMember.DataTableQrcode.Rows)
+                {
                
+                    row.test = "ddd";
+                }
 
-
-                     //set Processing Mode of Report as Local   
-                     ReportViewer1.ProcessingMode = ProcessingMode.Local;
-                    //set path of the Local report 
-
-                    // ReportViewer1.LocalReport.ReportPath = Server.MapPath("ReportQrcode.rdlc");
-                    ReportViewer1.LocalReport.ReportEmbeddedResource = "StockMUNA.ReportQrcode.rdlc";
-
-                    //creating object of DataSet dsmember and filling the DataSet using SQLDataAdapter   
-                    DataSetStock dsMember = new DataSetStock();
-                    MySqlConnection cn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["connection_stock"].ConnectionString);
-
-                    cn.Open();
-                    MySqlDataAdapter adapt = new MySqlDataAdapter("select stock_code,inv_no from stock_item where stock_code = 'TPS-009-002-001' ", cn);
-                    adapt.Fill(dsMember, "DataTableQrcode"); // DataTableQrcode DataSetQrcode
-                    cn.Close();
-
-                    //Providing DataSource for the Report   
-                    ReportDataSource rds = new ReportDataSource("DataSetQrcode", dsMember.Tables[0]);
-                    ReportViewer1.LocalReport.DataSources.Clear();
-                    //Add ReportDataSource   
-                    ReportViewer1.LocalReport.DataSources.Add(rds);
-
-
+                //Providing DataSource for the Report   
+                ReportDataSource rds = new ReportDataSource("DataSetQrcode", dsMember.Tables[0]);
+                ReportViewer1.LocalReport.DataSources.Clear();
+                //Add ReportDataSource   
+                ReportViewer1.LocalReport.DataSources.Add(rds);
+                ReportViewer1.LocalReport.Refresh();
 
                 //////////////////////////////////////////////////////// images path
 
 
-              //  ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/StockMUNA.ReportQrcode.rdlc");
-        
+                //  ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/StockMUNA.ReportQrcode.rdlc");
+
 
 
                 ReportViewer1.LocalReport.EnableExternalImages = true;
@@ -70,22 +103,66 @@ namespace StockMUNA
 
 
                // ReportViewer1.LocalReport.EnableExternalImages = true;
-                string imagePath = new Uri(Server.MapPath("images/qrcode/barc22.gif")).AbsoluteUri;
+                string imagePath = new Uri(Server.MapPath("images/qrcode/barc22.png")).AbsoluteUri;
                 ReportParameter parameter = new ReportParameter("ImagePath3", imagePath);
                 ReportViewer1.LocalReport.SetParameters(parameter);
                 ReportViewer1.LocalReport.Refresh();
 
 
+                //////////////////////////////////////////////////////// parameter byte
+               
+               
+                string result;
+                var content = "abc-589-cdf-757";
+                var writer = new BarcodeWriter
+                {
+                    Format = BarcodeFormat.CODE_128
+                };
+                var bitmap = writer.Write(content);
 
+                System.Drawing.Bitmap b = bitmap;
+                byte[] bytes;
+                using (System.IO.MemoryStream stream = new System.IO.MemoryStream())
+                {
+                    b.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    bytes = stream.ToArray();
+                    result = Convert.ToBase64String(bytes, 0, bytes.Length); ;
+                }
+
+
+                /////////////////////////////////////////////////////////////////////
+                ReportParameter bitmap_bar = new ReportParameter("bitmab_barcode", result);
+                ReportViewer1.LocalReport.SetParameters(bitmap_bar);
+                ReportViewer1.LocalReport.Refresh();
+
+
+
+
+            } // post back
+
+           /* DataSetStock dsMember2 = new DataSetStock();
+            DataTable tableadapt = new DataTable("DataTableQrcode");
+            foreach (DataRow dr in tableadapt.Rows) // search whole table
+            {
+                dr["test"] = "jkb"; //change the name 
+                dr["inv_no"] = "5565455"; //change the name 
+                
             }
-        } // end page load
+               dsMember2.Tables[0].AcceptChanges();*/
+
+
+
+
+
+
+        } //  end page load
 
 
 
 
          public void genqr()
         {
-            string code = "2255";
+            string code = "abc-589-cdf-757";
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
             QRCodeData qrCodeData = qrGenerator.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q);
             QRCode qrCode = new QRCode(qrCodeData);
@@ -109,7 +186,7 @@ namespace StockMUNA
 
 
                     imgBarCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
-                    string result = Convert.ToBase64String(byteImage, 0, byteImage.Length); ;
+                    string result = Convert.ToBase64String(byteImage, 0, byteImage.Length); 
 
 
                     //CreateImage(result.ToString());
@@ -122,55 +199,28 @@ namespace StockMUNA
 
 
 
-        public string CreateImage(string Byt)
-        {
-            try
-            {
-                byte[] data = Convert.FromBase64String(Byt);
 
-                var filename = Convert.ToString(System.Guid.NewGuid()).Substring(0, 5) + Convert.ToString(System.Guid.NewGuid()).Substring(0, 5) + System.DateTime.Now.ToString("FFFFFF") + System.DateTime.Now.Minute + ".png";// +System.DateTime.Now.ToString("fffffffffff") + ".png";
-                var file = HttpContext.Current.Server.MapPath("~/Images/qrcode/" + filename);
-                //var file = HttpContext.Current.Server.MapPath("~/AppImages/" + filename);
-                System.IO.File.WriteAllBytes(file, data);
-                string ImgName = ".../profileimages/" + filename;
-                
-                return filename;
-            }
-            catch (Exception e)
-            {
-                return "Error";
-            }
-
-        }
-
-
-
-        public void spire_barcode()
+        public void barcode()
         {
 
+            var content = "abc-589-cdf-757";
+            var writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.CODE_128
+            };
+            var bitmap = writer.Write(content);
 
-            /*  BarCode barcode = new BarCode();
-              barcode.Symbology = KeepAutomation.Barcode.Symbology.Code39;
-              barcode.CodeToEncode = "456123";
-              barcode.X = 10;
-               barcode.Y = 10;
-               barcode.TopMargin = 10;
-               barcode.BottomMargin = 10;
-               barcode.LeftMargin = 10;
-               barcode.RightMargin = 10;
-               barcode.DisplayText = true;
-               barcode.DisplayStartStop = true;
-               barcode.BarcodeUnit = KeepAutomation.Barcode.BarcodeUnit.Pixel;
-               barcode.Orientation = KeepAutomation.Barcode.Orientation.Degree0;
-               barcode.DPI = 72;
-              barcode.ImageFormat = ImageFormat.Tiff;
-              barcode.generateBarcodeToImageFile(Server.MapPath("Images/qrcode/barcode.png"));*/
+            System.Drawing.Bitmap b = bitmap;
+            byte[] bytes ;
+            using (System.IO.MemoryStream stream = new System.IO.MemoryStream())
+            {
+                bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                bytes = stream.ToArray();
+            }
 
-              /*  BarcodeSettings bs = new BarcodeSettings();
-                bs.Type = BarCodeType.Codabar;
-                bs.Data = "12345";
-                BarCodeGenerator bg = new BarCodeGenerator(bs);
-                bg.GenerateImage().Save(Server.MapPath("Images/qrcode/bacodes.png"));*/
+
+
+            // bitmap.Save(Server.MapPath("Images/qrcode/barc22.png"));
 
         }
 
